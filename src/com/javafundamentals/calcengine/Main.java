0e4643cc3f97834633481c0ba5f0debb41582816
Kalesh <kaleshsingh96@gmail.com>
2018-04-14 16:10:25 -0400

    Updated Calc Engine to use Loops and Switch

--- src/com/javafundamentals/calcengine/Main.java
+++ src/com/javafundamentals/calcengine/Main.java
@@ -3,25 +3,35 @@ package com.javafundamentals.calcengine;
 public class Main {
 	public static void main (String[] args) {
 
-		double val1 = 100.0d;
-		double val2 = 50.0d;
-		double result;
-		char opcode = 'd';
+		double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+		double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+		char[] opcodes = {'d', 'a', 's', 'm'};
+		double[] results = new double[opcodes.length];
 
-		if (opcode == 'a')
-			result = val1 + val2;
-		else if (opcode == 's')
-			result = val1 - val2;
-		else if (opcode == 'd')
-			result = val2 != 0.0d ? val1 / val2 : 0.0d;
-		else if (opcode == 'm') 
-			result = val1 * val2;
-		else {
-			System.out.println("Error - invalid opcode");
-			result = 0.0d;
+		for (int i = 0; i < opcodes.length; i++) {
+			switch (opcodes[i]) {
+				case 'a':
+					results[i] = leftVals[i] + rightVals[i];
+					break;
+				case 's':
+					results[i] = leftVals[i] - rightVals[i];
+					break;
+				case 'm':
+					results[i] = leftVals[i] * rightVals[i];
+					break;
+				case 'd':
+					results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+					break;
+				default:
+					System.out.println("Error - Invalid Opcode");
+					results[i] = 0.0d;
+					break;
+			}
 		}
 
-		System.out.println(result);
-
+		for (double result : results) {
+			System.out.print("Result = ");
+			System.out.println(result);
+		}
 	}
 }
