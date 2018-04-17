1fac4726827718499cda9d6653cdc786cd8bbbc7
Kalesh <kaleshsingh96@gmail.com>
2018-04-17 04:01:43 -0400

    Updated Calc Engine to use advanced data types

--- src/com/javafundamentals/calcengine/Main.java
+++ src/com/javafundamentals/calcengine/Main.java
@@ -55,6 +55,19 @@ public class Main {
 			System.out.print("Result = ");
 			System.out.println(calculator.getResult());
 		}
+		
+		System.out.println("\nUsing More Data Types\n");
+		String[] statements = {
+			"divide 100.0 50.0", 
+			"add 25.0 92.0",
+			"subtract 225.0 17.0",
+			"multiply 11.0 3.0"
+		};
 
+		CalculateHelper helper = new CalculateHelper();
+		for (String statement : statements) {
+			helper.process(statement);
+			System.out.println(helper);
+		}
 	}
 }
