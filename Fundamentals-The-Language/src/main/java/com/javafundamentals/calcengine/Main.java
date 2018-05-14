6fd1146f2e001d1e8b80e34edda14f0cd9788ff8
Kalesh <kaleshsingh96@gmail.com>
2018-05-13 23:20:20 -0400

    Added Power Of functionality using interfaces

--- Fundamentals-The-Language/src/main/java/com/javafundamentals/calcengine/Main.java
+++ Fundamentals-The-Language/src/main/java/com/javafundamentals/calcengine/Main.java
@@ -3,10 +3,14 @@ package com.javafundamentals.calcengine;
 public class Main {
 	public static void main (String[] args) {
 
-		String[] statements = {"add 25.0 92.0"};
+		String[] statements = {
+			"add 25.0 92.0", 
+			"power 5.0 2.0"
+		};
 
 		DynamicHelper helper = new DynamicHelper(new MathProcessing[] {
-			new Adder()
+			new Adder(), 
+			new PowerOf()
 		});
 
 		for (String statement : statements) {
